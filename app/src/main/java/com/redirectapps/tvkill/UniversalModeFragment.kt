66916d35e4987686babada80f7f06d47496b269e
Jonas L <jonas@determapp.de>
2018-06-06 16:34:13 +0200

    Update Copyright headers

--- app/src/main/java/com/redirectapps/tvkill/UniversalModeFragment.kt
+++ app/src/main/java/com/redirectapps/tvkill/UniversalModeFragment.kt
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2015 Sebastian Kappes
+ * Copyright (C) 2018 Jonas Lochmann
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
