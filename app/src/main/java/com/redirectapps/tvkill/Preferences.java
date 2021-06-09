a5f6b6da16623978533943e73a1da6f9178da57f
ysard <ysard@users.noreply.github.com>
2021-06-09 05:27:28 +0200

    Follow #49; Fix JSON loading... The filter on the mimetype application/json is in fact not supposed to work on Android which does not have this entry in its DB. But It works against all odds on some platforms. There is no way to my knowledge to detect this because MimeTypeMap does not contain this mimetype. It is therefore necessary to let the user choose any file and then do the usual input verification operations to be sure to have JSON.

--- app/src/main/java/com/redirectapps/tvkill/Preferences.java
+++ app/src/main/java/com/redirectapps/tvkill/Preferences.java
@@ -26,6 +26,16 @@ import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.provider.OpenableColumns;
+import android.webkit.MimeTypeMap;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import kotlin.text.Charsets;
 
 import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 
@@ -63,10 +73,20 @@ public class Preferences extends PreferenceActivity {
         filePicker.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                //Intent to start openIntents File Manager
+                // Intent to start openIntents File Manager
+                /* ACTION_OPEN_DOCUMENT is needed (contrary to ACTION_GET_CONTENT) because we need
+                 * long-term, persistent permission on the uri, not a one-time thing.
+                 * On some systems (like Xiaomi Redmi 4),
+                 * it will only authorize listing on downloads directory...
+                 */
                 Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
                 // Limit the type of files by mime-type: json only
-                intent.setType("application/json");
+                // But... Not all platforms/file browser (?) support it
+                if (MimeTypeMap.getSingleton().hasMimeType("application/json"))
+                    intent.setType("application/json");
+                else
+                    intent.setType("*/*");
+
                 /*
                  * Because we'll want to read the data of whatever file is picked,
                  * we set [Intent.CATEGORY_OPENABLE] to ensure this will succeed.
@@ -106,13 +126,36 @@ public class Preferences extends PreferenceActivity {
         super.onActivityResult(requestCode, resultCode, resultData);
 
         if (requestCode == OPEN_DOCUMENT_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-
+            // content://com.android.providers.downloads.documents/document/<number>
             Uri documentUri = resultData.getData();
             if (documentUri == null)
                 return;
 
+            String filename = getFileName(documentUri);
+            if (filename == null || !filename.endsWith(".json")) {
+                Toast.makeText(getApplicationContext(), R.string.json_not_valid, Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            // Test JSON validity
+            try {
+                InputStream stream = getContentResolver().openInputStream(documentUri);
+
+                byte[] buffer = new byte[0];
+                buffer = new byte[(int)stream.available()];
+                stream.read(buffer);
+                stream.close();
+
+                String jsonData = new String(buffer, Charsets.UTF_8);
+                JSONArray test = new JSONArray(jsonData);
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(), R.string.json_not_valid, Toast.LENGTH_SHORT).show();
+                return;
+            }
+
             // Persist the permission across restarts to allow us to reopen the document
-            MainActivity.getContext().getContentResolver().takePersistableUriPermission(
+            getContentResolver().takePersistableUriPermission(
                     documentUri,
                     Intent.FLAG_GRANT_READ_URI_PERMISSION
             );
@@ -123,7 +166,7 @@ public class Preferences extends PreferenceActivity {
                     documentUri.toString()
             ).apply();
 
-            this.findPreference("pattern_db_file").setSummary(getFileName(documentUri));
+            this.findPreference("pattern_db_file").setSummary(filename);
 
             // Restart the app/BrandContainer singleton
             MainActivity.restart();
@@ -134,17 +177,11 @@ public class Preferences extends PreferenceActivity {
     public String getFileName(Uri uri) {
         String result = null;
         if (uri.getScheme().equals("content")) {
-            try (Cursor cursor = getContentResolver().query(uri, null, null, null, null)) {
+            // projection: Optional but bad for performance if empty
+            try (Cursor cursor = getContentResolver().query(uri, new String[]{OpenableColumns.DISPLAY_NAME}, null, null, null)) {
                 if (cursor != null && cursor.moveToFirst()) {
                     result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
                 }
-            }
-        }
-        if (result == null) {
-            result = uri.getPath();
-            int cut = result.lastIndexOf('/');
-            if (cut != -1) {
-                result = result.substring(cut + 1);
             }
         }
         return result;
